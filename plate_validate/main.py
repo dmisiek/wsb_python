b9278125ba40f589e58907b65347efa5933039f0
dmisiek <dominikmisiek02@gmail.com>
2023-12-28 11:50:55 +0100

    Created plate validators for common cars & motors

--- plate_validate/main.py
+++ plate_validate/main.py
@@ -1,82 +1,111 @@
+import os
+from typing import Optional
+
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
-# import matplotlib.pyplot as plt
 import easyocr
-import pytesseract
 
-from plate_validate.plate_validator import TwoDigitDiscriminantPlateValidator, ThreeDigitDiscriminantPlateValidator
+from plate_validate.plate_validator import ThreeDigitRegionCodePlateValidator, TwoDigitRegionCodePlateValidator, \
+    ReducedPlateValidator, MotorPlateValidator
 
-pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+def main():
+    image_path = 'example.png'
 
-IMAGE_PATH = 'example.png'
+    if not os.path.isfile(image_path):
+        raise FileNotFoundError(f'{image_path} not exists or path is incorrect.')
 
-def detect_and_cut_plate(path: str):
+    print("Start detecting car's plate...")
+    cropped_path = detect_and_cut_plate(image_path)
+
+    if cropped_path is None:
+        print("No car's plate detected")
+        return
+
+    print(f"Potential car plate detected and saved [{cropped_path}]")
+
+    print("Start recognizing plate license numbers...")
+    plate = recognize_plate_numbers(cropped_path)
+
+    print(f"Validating plate '{plate}'...")
+    result = 'valid' if validate_plate(plate) else 'invalid'
+
+    print(f'Plate {plate} is {result}')
+
+def detect_and_cut_plate(path: str) -> Optional[str]:
     img = cv2.imread(path, cv2.IMREAD_COLOR)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # blur image for easier shapes recognition
     gray = cv2.bilateralFilter(gray, 13, 15, 15)
 
-    # transforming image for countour
+    # transforming image for contours
     edged = cv2.Canny(gray, 30, 200)
 
-    contours, _ = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)
-    plate_countour = None
+    # preparing contours
+    img_contours, _ = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    img_contours = sorted(img_contours, key=cv2.contourArea, reverse=True)
+    plate_contour = None
 
-    for c in contours:
+    # searching for 4 vertices polygon
+    for c in img_contours:
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.018 * peri, True)
 
         if len(approx) == 4:
-            plate_countour = approx
+            plate_contour = approx
             break
 
-    if plate_countour is None:
-        print("No plate detected")
+    if plate_contour is None:
         return None
 
     # creating mask and fill with shape of plate
     mask = np.zeros(gray.shape, np.uint8)
-    cv2.drawContours(mask, [plate_countour], 0, (255, 255, 255), -1)
+    cv2.drawContours(mask, [plate_contour], 0, (255, 255, 255), -1)
 
     # getting coords where mask is white
     x, y = np.where(mask == 255)
-    topX, topY = (np.min(x), np.min(y))
-    bottomX, bottomY = (np.max(x), np.max(y))
+    top_x, top_y = (np.min(x), np.min(y))
+    bottom_x, bottom_y = (np.max(x), np.max(y))
 
     # crop and save image
-    cropped_image = gray[topX:bottomX, topY:bottomY]
+    cropped_image = gray[top_x:bottom_x, top_y:bottom_y]
     cropped_path = f'{path.split(".")[0]}_cropped.png'
     cv2.imwrite(cropped_path, cropped_image)
 
     return cropped_path
 
-def recognize_plate_numbers(path: str):
+def recognize_plate_numbers(path: str) -> str:
     img = cv2.imread(path, cv2.IMREAD_COLOR)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # thresholding image for better recognition with ocr
     _, thresh = cv2.threshold(gray, 72, 255, cv2.THRESH_BINARY)
 
-    # EasyOCR
     reader = easyocr.Reader(['en'])
     results = reader.readtext(thresh, detail=0)
-    return ''.join(results)
 
-    # pytesseract
-    # result = pytesseract.image_to_string(thresh, config='--psm 7')
-    # return result
+    car_plate = ''.join(results).replace(' ', '')
+    car_plate.capitalize()
 
-# TODO: Implement plate validation
-def validate_plate(plate: str):
-    validator = ThreeDigitDiscriminantPlateValidator(plate)
-    print(validator, validator.validate())
+    return car_plate
 
-# TODO: Uncomment after plate validator will be full implemented
-# cropped_path = detect_and_cut_plate(IMAGE_PATH)
-# plate = recognize_plate_numbers(cropped_path)
-# print(plate)
+def validate_plate(car_plate: str):
+    # TODO: Add validator for TemporaryPlates, SafetyInstitutions
+    for validator in [
+        ThreeDigitRegionCodePlateValidator,
+        TwoDigitRegionCodePlateValidator,
+        MotorPlateValidator,
+        ReducedPlateValidator,
+    ]:
+        try:
+            v = validator(car_plate)
+            valid = v.validate()
 
-plate_validation = validate_plate('XY1AA04')
+            if valid:
+                return True
+        except AssertionError:
+            pass
+
+    return False
+
+main()
--- plate_validate/plate_validator.py
+++ plate_validate/plate_validator.py
@@ -1,78 +1,154 @@
+import os.path
 from abc import ABC, abstractmethod
 import re
 
 class PlateValidator(ABC):
-    full_plate: str
-    discriminant: str
     plate: str
 
-    def __init__(self, full_plate):
-        self.full_plate = full_plate
+    def __init__(self, plate):
+        self.plate = plate
 
     def __str__(self):
-        return f'Plate: ({self.discriminant} {self.plate})'
+        return f'Plate: ({self.plate})'
 
     @abstractmethod
-    def validate(self) -> bool:
+    def get_correct_plate_expressions(self) -> list[str]:
         raise NotImplementedError
 
-class TwoDigitDiscriminantPlateValidator(PlateValidator):
-    correct_plates = (
-        "^\d{5}$",
-        "^\d{4}[A-Z]{1}$",
-        "^\d{3}[A-Z]{2}$",
-        "^\d{1}[A-Z]{1}[1-9]{1}\d{2}$",
-        "^\d{1}[A-Z]{2}[1-9]{1}\d{1}$",
-    )
+    @abstractmethod
+    def is_correct_length(self) -> bool:
+        raise NotImplementedError
 
-    def __init__(self, full_plate):
-        super().__init__(full_plate)
-
-        assert len(full_plate) == 7, "Two digit discriminant plate must be 7 chars long"
-
-        self.discriminant = full_plate[:2]
-        self.plate =full_plate[2:]
+    @abstractmethod
+    def is_correct_region_code(self) -> bool:
+        raise NotImplementedError
 
     def validate(self) -> bool:
-        if re.search(r"[A-Z]{2}", self.discriminant) is None:
+        if not self.is_correct_length():
             return False
 
-        for expression in self.correct_plates:
+        if not self.is_correct_region_code():
+            return False
+
+        for expression in self.get_correct_plate_expressions():
             result = re.search(expression, self.plate)
             if result is not None:
                 return True
+
         return False
 
-class ThreeDigitDiscriminantPlateValidator(PlateValidator):
-    correct_plates = (
-        "^[A-Z]{1}\d{3}$",
-        "^\d{2}[A-Z]{2}$",
-        "^\d{1}[A-Z]{1}[1-9]{1}\d{1}$",
-        "^\d{2}[A-Z]{1}[1-9]{1}$",
-        "^[1-9]{1}[A-Z]{2}[1-9]{1}$",
-        "^[A-Z]{2}\d{2}$",
-        "^\d{5}$",
-        "^\d{4}[A-Z]{1}$",
-        "^\d{3}[A-Z]{2}$",
-        "^[A-Z]{1}\d{2}[A-Z]{1}$",
-        "^[A-Z]{1}[1-9]{1}[A-Z]{2}$",
-    )
+class TwoDigitRegionCodePlateValidator(PlateValidator):
+    def __init__(self, plate):
+        assert os.path.isfile('two-digit-region-codes.txt'), "TwoDigitRegionCodePlateValidator require `two-digit-region-codes.txt` file to work properly"
+        super().__init__(plate)
 
-    def __init__(self, full_plate):
-        super().__init__(full_plate)
+    def get_correct_plate_expressions(self) -> list:
+        return [
+            "^[A-Z]{2}\d{5}$",
+            "^[A-Z]{2}\d{4}[ACE-HJ-NP-Y]{1}$",
+            "^[A-Z]{2}\d{3}[ACE-HJ-NP-Y]{2}$",
+            "^[A-Z]{2}\d{1}[ACE-HJ-NP-Y]{1}[1-9]{1}\d{2}$",
+            "^[A-Z]{2}\d{1}[ACE-HJ-NP-Y]{2}[1-9]{1}\d{1}$",
+        ]
 
-        assert len(full_plate) == 7 or len(full_plate) == 8, "Three digit discriminant plate can be 7 or 8 chars long"
+    def is_correct_length(self) -> bool:
+        return len(self.plate) == 7
 
-        self.discriminant = full_plate[:3]
-        self.plate =full_plate[3:]
+    def is_correct_region_code(self) -> bool:
+        with open('two-digit-region-codes.txt', mode='r') as f:
+            region_codes = f.read().splitlines()
 
-    def validate(self) -> bool:
-        if re.search(r"[A-Z]{3}", self.discriminant) is None:
-            return False
+        region_code = self.plate[:2]
+        return region_code in region_codes
 
-        for expression in self.correct_plates:
-            result = re.search(expression, self.plate)
-            if result is not None:
-                return True
-        return False
+class ThreeDigitRegionCodePlateValidator(PlateValidator):
+    def __init__(self, plate):
+        assert os.path.isfile('three-digit-region-codes.txt'), "ThreeDigitRegionCodePlateValidator require `three-digit-region-codes.txt` file to work properly"
+        super().__init__(plate)
 
+    def get_correct_plate_expressions(self) -> list:
+        return [
+            "^[A-Z]{3}[ACE-HJ-NP-Y]{1}\d{3}$",
+            "^[A-Z]{3}\d{2}[ACE-HJ-NP-Y]{2}$",
+            "^[A-Z]{3}\d{1}[ACE-HJ-NP-Y]{1}[1-9]{1}\d{1}$",
+            "^[A-Z]{3}\d{2}[ACE-HJ-NP-Y]{1}[1-9]{1}$",
+            "^[A-Z]{3}[1-9]{1}[ACE-HJ-NP-Y]{2}[1-9]{1}$",
+            "^[A-Z]{3}[ACE-HJ-NP-Y]{2}\d{2}$",
+            "^[A-Z]{3}\d{5}$",
+            "^[A-Z]{3}\d{4}[ACE-HJ-NP-Y]{1}$",
+            "^[A-Z]{3}\d{3}[ACE-HJ-NP-Y]{2}$",
+            "^[A-Z]{3}[ACE-HJ-NP-Y]{1}\d{2}[ACE-HJ-NP-Y]{1}$",
+            "^[A-Z]{3}[ACE-HJ-NP-Y]{1}[1-9]{1}[ACE-HJ-NP-Y]{2}$",
+        ]
+
+    def is_correct_length(self) -> bool:
+        return len(self.plate) in [7, 8]
+
+    def is_correct_region_code(self) -> bool:
+        with open('three-digit-region-codes.txt', mode='r') as f:
+            region_codes = f.read().splitlines()
+
+        region_code = self.plate[:3]
+        return region_code in region_codes
+
+class MotorPlateValidator(PlateValidator):
+    def __init__(self, plate):
+        assert os.path.isfile('two-digit-region-codes.txt'), "TwoDigitRegionCodePlateValidator require `two-digit-region-codes.txt` file to work properly"
+        assert os.path.isfile('three-digit-region-codes.txt'), "ThreeDigitRegionCodePlateValidator require `three-digit-region-codes.txt` file to work properly"
+        super().__init__(plate)
+
+    def get_correct_plate_expressions(self) -> list:
+        return [
+            "^[A-Z]{2}\d{4}$",
+            "^[A-Z]{2}\d{3}[ACE-HJ-NP-Y]{1}$",
+            "^[A-Z]{2}\d{2}[ACE-HJ-NP-Y]{1}[1-9]{1}$",
+            "^[A-Z]{2}[1-9]{1}[ACE-HJ-NP-Y]{1}\d{2}$",
+            "^[A-Z]{2}[ACE-HJ-NP-Y]{1}\d{3}$",
+            "^[A-Z]{2}\d{2}[ACE-HJ-NP-Y]{2}$",
+            "^[A-Z]{2}[1-9]{1}[ACE-HJ-NP-Y]{2}[1-9]{1}$",
+            "^[A-Z]{2}[ACE-HJ-NP-Y]{2}\d{2}$",
+            "^[A-Z]{3}[ACE-HJ-NP-Y]{1}\d{3}$",
+            "^[A-Z]{3}\d{2}[ACE-HJ-NP-Y]{2}$",
+            "^[A-Z]{3}\d{1}[ACE-HJ-NP-Y]{1}[1-9]{1}\d{1}$",
+            "^[A-Z]{3}\d{2}[ACE-HJ-NP-Y]{1}[1-9]{1}$",
+            "^[A-Z]{3}[1-9]{1}[ACE-HJ-NP-Y]{2}[1-9]{1}$",
+            "^[A-Z]{3}[ACE-HJ-NP-Y]{2}\d{2}$",
+            "^[A-Z]{3}[ACE-HJ-NP-Y]{1}\d{2}[ACE-HJ-NP-Y]{1}$",
+            "^[A-Z]{3}[ACE-HJ-NP-Y]{1}[1-9]{1}[ACE-HJ-NP-Y]{2}$",
+        ]
+
+    def is_correct_length(self) -> bool:
+        return len(self.plate) in [6, 7]
+
+    def is_correct_region_code(self) -> bool:
+        with open('two-digit-region-codes.txt', mode='r') as f:
+            region_codes = f.read().splitlines()
+
+        region_code = self.plate[:2]
+        if region_code in region_codes:
+            return True
+
+        with open('three-digit-region-codes.txt', mode='r') as f:
+            region_codes = f.read().splitlines()
+
+        region_code = self.plate[:3]
+        return region_code in region_codes
+
+class ReducedPlateValidator(PlateValidator):
+    def get_correct_plate_expressions(self) -> list:
+        return [
+            "^[A-Z]{1}\d{3}$",
+            "^[A-Z]{1}\d{2}[ACE-HJ-NP-Y]{1}$",
+            "^[A-Z]{1}[1-9]{1}[ACE-HJ-NP-Y]{1}[1-9]{1}$",
+            "^[A-Z]{1}[ACE-HJ-NP-Y]{1}\d{2}$",
+            "^[A-Z]{1}[1-9]{1}[ACE-HJ-NP-Y]{2}$",
+            "^[A-Z]{1}[ACE-HJ-NP-Y]{2}[1-9]{1}$",
+            "^[A-Z]{1}[ACE-HJ-NP-Y]{1}[1-9]{1}[ACE-HJ-NP-Y]{1}$",
+        ]
+
+    def is_correct_length(self) -> bool:
+        return len(self.plate) == 4
+
+    def is_correct_region_code(self) -> bool:
+        region_code = self.plate[:1]
+        return re.search('^[B-GI-PR-TV-Z]$', region_code) is not None
